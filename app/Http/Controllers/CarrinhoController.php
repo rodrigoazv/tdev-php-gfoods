50e06b006c8649cee24d2f02fa3002697ac4719e
roazv <rodrigozcma@gmail.com>
2020-11-30 10:24:12 -0300

    Adding pedidos produtos

--- app/Http/Controllers/CarrinhoController.php
+++ app/Http/Controllers/CarrinhoController.php
@@ -5,22 +5,28 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Pedido;
+use App\PedidoProduto;
 
 class CarrinhoController extends Controller
 {
     private $objPedido;
+    private $objPedidoProduto;
     public function __construct(){
         $this->objPedido = new Pedido();
+        $this->objPedidoProduto = new PedidoProduto();
     }
     public function index(){
         $pedidos = $this->objPedido::where([
             'status' => 'FEITO',
             'users_id' => Auth::id()
         ])->get();
+        $pedidos_p = $this->objPedidoProduto::where([
+            'pedido_id' => $pedidos[0]->id,
+        ])->get();
 
         dd([
             $pedidos,
-            $pedidos[0]->pedido_produtos, 
+            $pedidos_p[0]->produto_id,   
         ]);
 
         return view('carrinho.index', compact('pedidos'));
