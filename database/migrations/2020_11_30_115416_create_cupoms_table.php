7bed5c2c559012675cc14864043d481d3c9b2c8f
roazv <rodrigozcma@gmail.com>
2020-11-30 09:02:34 -0300

    adding

---
+++ database/migrations/2020_11_30_115416_create_cupoms_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCupomsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cupoms', function (Blueprint $table) {
+            $table->id();
+            $table->string('email');
+            $table->double('valor');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cupoms');
+    }
+}
