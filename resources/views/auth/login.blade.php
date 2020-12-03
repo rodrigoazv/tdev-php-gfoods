22b7cbae71709a91ab9387954015f4c47957a015
roazv <rodrigozcma@gmail.com>
2020-12-03 06:44:38 -0300

    Change Layout

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,12 +1,71 @@
-@extends('layouts.app')
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-@section('content')
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+    <script type='text/javascript' src="{{ asset('js/carrinho.js') }}" defer></script>
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <style>
+    
+.sidenav {
+    height: 100vh;
+    background-color: #000;
+    overflow-x: hidden;
+    padding-top: 20px;
+}
+
+.banner-home{
+    width: 100%;
+    height: 300px;
+    background-image: url(https://johnnyrockets.com.br/wp-content/uploads/2020/09/banner_site-LTO3_2020-vegan_1920x670-min-1024x358.jpg);
+    object-fit: cover;
+}
+.container{
+    margin: 0;
+    padding: 0;
+    display: flex;
+    justify-content: space-between;
+    height:100%;
+}
+.banner{
+    background-image: url(https://images8.alphacoders.com/809/809022.jpg);
+}
+.login{
+    width: 500px;
+    margin: 100px 0 0 0;
+}
+.login-main-text{
+    margin-top: 20%;
+    padding: 60px;
+    color: #fff;
+}
+</style>
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
+    <div class="sidenav banner">
+        <div class="login-main-text" >
+           <h2>Com a Go Fods você faz seu<br/> pedido sem complicações</h2>
+           <p>Faça seu login ou solicite registro.</p>
+        </div>
+     </div>
+    <div class="main login">
+        <div class="col-md-12">
                 <div class="card-header">{{ __('Login') }}</div>
-
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
@@ -64,10 +123,10 @@
                                 @endif
                             </div>
                         </div>
+                        <a href="register">Sem Login ?</a>
                     </form>
                 </div>
-            </div>
+           
         </div>
     </div>
 </div>
-@endsection
